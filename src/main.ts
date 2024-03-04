3981a20f5503d89c3615a8386e742728190ea6df
lxy <lxy2914344878@163.com>
2024-03-04 11:04:38 +0800

    完成登录持久化

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/css/satoshi.css'
 import './assets/css/style.css'
 import 'jsvectormap/dist/css/jsvectormap.min.css'
 import 'flatpickr/dist/flatpickr.min.css'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueApexCharts from 'vue3-apexcharts'
@@ -13,8 +13,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(VueApexCharts)
 app.use(ElementPlus)
